49dbfd0cfdf51e889ed2556fd30bc5025ab5c871
Nur Alam <nuralam1122345@gmail.com>
2026-07-26 12:21:24 +0600

    problem08

--- problem_04.js
+++ problem_04.js
@@ -16,32 +16,34 @@ loop through number 1 to n , if a number is divisible by 3 , print 'fizz' ; if d
 //   }
 // }
 
-// function fizzBuzz(number) {
-//   for (let i = 1; i <= number; i++) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//       return "FizzBuzz";
-//     } else if (i % 3 === 0) {
-//       return "Fizz";
-//     } else if (i % 5 === 0) {
-//       return "Buzz";
-//     } else {
-//       return i;
-//     }
-//   }
-// }
-// console.log(fizzBuzz(30));
-
 function fizzBuzz(number) {
+   
   for (let i = 1; i <= number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
+      return "FizzBuzz";
     } else if (i % 3 === 0) {
-      console.log("Fizz");
+      return "Fizz";
     } else if (i % 5 === 0) {
-      console.log("Buzz");
+      return "Buzz";
     } else {
-        console.log(i)
+        console.log("number")
+      return i;
     }
   }
 }
-fizzBuzz(30);
+console.log(fizzBuzz(30));
+
+// function fizzBuzz(number) {
+//   for (let i = 1; i <= number; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//         console.log(i)
+//     }
+//   }
+// }
+// fizzBuzz(30);
---
+++ problem_08.js
@@ -0,0 +1,9 @@
+// let str = "my name is nur alam";
+// let strArray = str.split(" ");
+// console.log(strArray.length);
+
+function wordCount(str) {
+  let strArray = str.split(" ");
+  return strArray.length
+}
+console.log(wordCount("my name is nur"))
