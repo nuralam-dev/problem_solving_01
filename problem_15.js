4234542aedcd8684d125b165b2205d38e973150a
Nur Alam <nuralam1122345@gmail.com>
2026-07-27 22:07:58 +0600

    solved all

--- problem_15.js
+++ problem_15.js
@@ -1,38 +1,45 @@
-let arr = [12, 13, 18, 19, 20, 22, 28, 26];
-// let evenNumber = [];
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] % 2 === 0) {
-//     // console.log(arr[i]);
-//     evenNumber.push(arr[i]);
+// let arr = [12, 13, 18, 19, 20, 22, 28, 26];
+// // let evenNumber = [];
+// // for (let i = 0; i < arr.length; i++) {
+// //   if (arr[i] % 2 === 0) {
+// //     // console.log(arr[i]);
+// //     evenNumber.push(arr[i]);
+// //   }
+// // }
+// // console.log(evenNumber);
+// // let sum = 0;
+// // for (let key of evenNumber) {
+// //   sum += key;
+// // }
+// // console.log(sum);
+
+// // with function
+
+// function getEvenNumbers(arr) {
+//   let evenNumber = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       evenNumber.push(arr[i]);
+//     }
 //   }
+
+//   return evenNumber;
 // }
-// console.log(evenNumber);
-// let sum = 0;
-// for (let key of evenNumber) {
-//   sum += key;
+// function getSum(arr) {
+//   let sum = 0;
+//   for (let key of arr) {
+//     sum += key;
+//   }
+//   return sum;
 // }
+// let evenNumbers = getEvenNumbers(arr);
+// let sum = getSum(evenNumbers);
 // console.log(sum);
 
-// with function
-
-function getEvenNumbers(arr) {
-  let evenNumber = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      evenNumber.push(arr[i]);
-    }
-  }
-
-  return evenNumber;
+let age = 18;
+if (age > 18) {
+ console.log("Adult");
+} else {
+ console.log("Minor");
 }
-function getSum(arr) {
-  let sum = 0;
-  for (let key of arr) {
-    sum += key;
-  }
-  return sum;
-}
-let evenNumbers = getEvenNumbers(arr);
-let sum = getSum(evenNumbers);
-console.log(sum);
